6ee91757cc5792f37f4002323af6c01154d41384
yerdaulet <mussabek.yerdaulet@gmal.com>
2026-06-11 16:37:58 +0500

    First Commit

---
+++ Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yerdaulet Mussabek on 11.06.2026.
+//
+
+#include "Matrix.h"
---
+++ Matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by Yerdaulet Mussabek on 11.06.2026.
+//
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+
+
+class Matrix {
+
+};
+
+
+
+#endif //MATRIX_H
